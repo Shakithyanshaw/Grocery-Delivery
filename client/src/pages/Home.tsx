4b7c726427c070f8b7575ffaca626c80bdb2df86
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-06-24 15:03:48 +0530

    complete hero component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import Hero from '../components/Home/Hero';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
+    <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <Hero />
     </div>
   );
 };
