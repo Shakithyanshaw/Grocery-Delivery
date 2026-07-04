a196798753cc1979df620b92ffe352525f2a9894
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-07-04 11:07:47 +0530

    feat: implement My Orders page with order fetching and cart clearing functionality

--- client/src/pages/MyOrders.tsx
+++ client/src/pages/MyOrders.tsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from 'react';
+import type { Order } from '../types';
+import { useSearchParams } from 'react-router';
+import { useCart } from '../context/CartContext';
+import { dummyDashboardOrdersData } from '../assets/assets';
+
 const MyOrders = () => {
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || '$';
+
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState('all');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const tabs = ['all', 'Placed', 'Out for Delivery', 'Delivered'];
+
+  const { clearCart } = useCart();
+
+  const fetchOrders = async () => {
+    setOrders(dummyDashboardOrdersData as any);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (searchParams.get('clearCart')) {
+      clearCart();
+      setSearchParams({});
+      setTimeout(() => {
+        fetchOrders();
+      }, 2000);
+    } else {
+      fetchOrders();
+    }
+  }, [activeTab]);
+
   return (
     <div>
       <h1>Welcome to the My Orders Page!</h1>
