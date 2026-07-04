ee9af48db4b4d9c9ecd718e6e6a4bc5039815cff
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-07-04 12:06:21 +0530

    feat: implement Order Tracking page with order fetching and loading state

--- client/src/pages/OrderTracking.tsx
+++ client/src/pages/OrderTracking.tsx
@@ -1,4 +1,26 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import type { Order } from '../types';
+import { useEffect, useState } from 'react';
+import { dummyDashboardOrdersData } from '../assets/assets';
+import Loading from '../components/Loading';
+
 const OrderTracking = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [order, setOrder] = useState<Order | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [liveLocation, setLiveLocation] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
+
+  useEffect(() => {
+    setOrder(dummyDashboardOrdersData.find((o) => o._id === id) as any);
+    setLoading(false);
+  }, [id, navigate]);
+
+  if (loading) return <Loading />;
+  if (!order) null;
   return (
     <div>
       <h1>Welcome to the Order Tracking Page!</h1>
