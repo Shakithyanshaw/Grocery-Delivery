e9b70ec91f6babc51d0959d2e1be2a05fada4c08
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-07-29 11:41:06 +0530

    feat: add inngest integration and nodemailer dependency

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,8 @@ import authRouter from './routes/authRouter.js';
 import productRouter from './routes/productRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import { serve } from 'inngest/express';
+import { inngest, functions } from './inngest/index.js';
 
 const app = express();
 
@@ -21,6 +23,8 @@ app.use('/api/auth', authRouter);
 app.use('/api/products', productRouter);
 app.use('/api/upload', uploadRouter);
 app.use('/api/orders', orderRouter);
+
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 //Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
