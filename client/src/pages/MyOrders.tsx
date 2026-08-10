a7f7cf8e2d57c9976a1dde47c792624911a30f52
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-08-11 00:41:44 +0530

    feat: replace dummy order data with API integration in MyOrders component

--- client/src/pages/MyOrders.tsx
+++ client/src/pages/MyOrders.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import type { Order } from '../types';
 import { useSearchParams } from 'react-router';
 import { useCart } from '../context/CartContext';
-import { dummyDashboardOrdersData, statusColors } from '../assets/assets';
+import { statusColors } from '../assets/assets';
 import Loading from '../components/Loading';
 import { CalendarIcon, ChevronRightIcon, PackageIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import api from '../config/api';
 
 const MyOrders = () => {
   const currency = import.meta.env.VITE_CURRENCY_SYMBOL || '$';
@@ -20,8 +22,16 @@ const MyOrders = () => {
   const { clearCart } = useCart();
 
   const fetchOrders = async () => {
-    setOrders(dummyDashboardOrdersData as any);
-    setLoading(false);
+    setLoading(true);
+    try {
+      const params = activeTab !== 'all' ? `?status=${activeTab}` : '';
+      const { data } = await api.get(`/orders${params}`);
+      setOrders(data.orders);
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || error?.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
